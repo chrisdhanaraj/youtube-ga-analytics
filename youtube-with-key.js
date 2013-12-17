2758e4a610f07892f690d5ef7b26de076ee783d6
Chris Dhanaraj <chris.dhanaraj@thomsonreuters.com>
2013-12-17 13:59:41 -0500

    Attribution

--- youtube-with-key.js
+++ youtube-with-key.js
@@ -1,3 +1,9 @@
+// --------------------
+// YouTube Analytics
+// by: @chrisdhanaraj
+// Inspired by http://www.lunametrics.com/blog/2012/10/22/automatically-track-youtube-videos-events-google-analytics
+// --------------------
+
 /* globals YT, _gaq */
 
 (function (window, $) {
